57c90c69d6964566a04916c1747b2b71efb883b5
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-09-07 04:08:06 +0900

    fix: error 수정_firebase 재설치

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,12 @@ const firebaseConfig = {
     // config 정보
     apiKey: "AIzaSyA16oVFJ4dKPMIbrfyJQ9o63dFZ_cz37FA",
     authDomain: "tmm-web-67a2f.firebaseapp.com",
+    databaseURL: "https://tmm-web-67a2f-default-rtdb.firebaseio.com",
     projectId: "tmm-web-67a2f",
     storageBucket: "tmm-web-67a2f.appspot.com",
     messagingSenderId: "257906377885",
-    appId: "1:257906377885:web:d5583ab80b7b881c5b4074",
-    measurementId: "G-CT7CCX138R"
+    appId: "1:257906377885:web:c137c702917bcea25b4074",
+    measurementId: "G-CHJKEG2BDR"
 };
 
 // firebaseConfig 정보로 firebase 시작
@@ -19,4 +20,4 @@ firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 
 // 필요한 곳에서 사용할 수 있도록 내보내기
-export { firestore };
+export {firestore};
