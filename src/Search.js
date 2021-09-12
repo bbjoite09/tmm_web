20d8dab89c8f3f2a38c09444a1f733adbf044f23
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-09-13 01:21:41 +0900

    feat: 오류 수정

--- src/Search.js
+++ src/Search.js
@@ -1,66 +1,46 @@
-import React from "react";
-import styled from "styled-components";
-import line1 from "./image/selected_line1.svg"
-import line2 from "./image/selected_line2.svg"
-import line3 from "./image/selected_line3.svg"
-import line4 from "./image/selected_line4.svg"
-import line5 from "./image/selected_line5.svg"
-import line6 from "./image/selected_line6.svg"
-import line7 from "./image/selected_line7.svg"
-import line8 from "./image/selected_line8.svg"
-import line9 from "./image/selected_line9.svg"
-import {Link} from "react-router-dom";
-
-import name_list from "./mystation.json"
-import {useSelector} from "react-redux";
+import React from 'react';
+import Divider from '@material-ui/core/Divider';
 import * as Hangul from 'hangul-js';
-import {render} from "react-dom";
+import name_list from "./mystation.json";
 
-const Search = () => {
-    const danger_list = useSelector(state => state.danger.list);
 
+const Search = (props) => {
     const search_list = [...name_list];
-    // console.log(search_list[0])
 
+    const items = search_list.filter((data) => {
+        var str = ""
+        if (props.search == null) {
+            return
+        } else if (data.stationName.includes(props.text.current.value) || data.diassembled.includes(Hangul.disassemble(this.text.current.value).join(""))) {
+            return data
+        } else if (props.state.search === "") {
+            this.props.history.push("/searchNone")
+        }
 
-    // object에 초성필드 dissembled 추가
-    search_list.forEach(function (item) {
-        var dis = Hangul.disassemble(item.stationName, true);
-        var cho = dis.reduce(function (prev, elem) {
-            elem = elem[0] ? elem[0] : elem;
-            return prev + elem;
-        }, "");
-        item.diassembled = cho;
-    });
-
-    // console.log(search_list);
-
-    const items = search_list.map(data => {
-        return (
-            <div>
-                <ul>
-                    <li style={{position: 'relative', left: '10vh'}}>
-                        <span>{data.stationName}</span>
-                    </li>
-                </ul>
-            </div>
-        )
     })
+        .map(data => {
+            return (
+                <div>
+                    <div style={{
+                        width: "100%",
+                        height: "41px",
+                        display: "flex",
+                        alignItems: "center"
+                    }} onClick={() => {
+                        this.props.history.push('/details=' + data.lineNum[0] + data.stationName)
+                    }}>
+                        <p style={{
+                            alignmentBaseline: "center",
+                            position: 'relative',
+                            left: "10vh",
+                            marginLeft: "20%"
+                        }}>{data.lineNum} - {data.stationName}</p>
 
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter item to be searched" onChange={(e) => this.searchSpace(e)}/>
-            {
-                search_list.map(data => {
-                    return (
-                        <div>
-                            {items}
-                        </div>
-                    )
-                })}
-        </div>
-    )
+                    </div>
+                    <Divider/>
+                </div>
+            )
+        })
 
 
 };
